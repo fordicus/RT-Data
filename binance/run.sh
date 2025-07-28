ea3d1b66fe90275b2d5c862e5a0992bfb16b7a05
Robinus <fordicus@naver.com>
2025-07-28 20:12:31 +0200

    f1-50-7c 2025-07-28 20:12:31.255471:

--- binance/run.sh
+++ binance/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-sudo chrt -f 80 nice -n -19 ionice -c1 -n0 ./stream_binance
+# sudo chrt -f 80 nice -n -19 ionice -c1 -n0 ./stream_binance
+sudo chrt -f 80 nice -n -19 ionice -c1 -n0 $(which python) stream_binance.py
