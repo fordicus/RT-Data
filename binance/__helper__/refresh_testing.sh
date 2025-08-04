40127289405f08daefbb3c0dc12f447c12ab1084
Robinus <fordicus@naver.com>
2025-08-04 21:57:44 +0200

    f1-50-7c 2025-08-04 21:57:44.557239:

--- binance/__helper__/refresh_testing.sh
+++ binance/__helper__/refresh_testing.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+# refresh_testing.sh
 
 clear
+# sudo rm -rf data
+# sudo rm -rf stream_binance
+# sudo rm -rf stream_binance.log
 
+# Re-clone the Repository
 CURRENT_USER=$(whoami)
 cd /home/$CURRENT_USER
-
 sudo rm -rf /home/$CURRENT_USER/RT-Data
-
 git clone https://github.com/fordicus/RT-Data.git
-
-cd /home/$CURRENT_USER/RT-Data/binance/
-
-# python /home/$CURRENT_USER/RT-Data/binance/stream_binance.py
